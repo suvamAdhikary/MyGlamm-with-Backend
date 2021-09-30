8ddc7f4415a1fd246e9710efd920a40e397a4ebe
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-09-30 16:53:18 +0530

    product page and fixed controller

--- app/src/controllers/category.controller.js
+++ app/src/controllers/category.controller.js
@@ -8,8 +8,24 @@ const crudController = require('./crud.controller');
 
 router.post('', crudController.post(Category));
 router.get('', crudController.get(Category));
-router.get('/:id', crudController.getOne(Category));
+// router.get('/:id', crudController.getOne(Category));
 router.patch('/:id', crudController.updateOne(Category));
 router.delete('/:id', crudController.deleteOne(Category));
+
+
+// router.get('/:id', async (req, res) => {
+    
+//     try {
+//         const category = await Category.findById(req.params.id).lean().exec();
+//         console.log(category);
+//         res.render('pages/category.ejs', {
+//             category
+//         });
+//     } catch (err) {
+//         return res.status(400).send({err})
+//     }
+// });
+
+
 
 module.exports = router;
--- app/src/controllers/product.controller.js
+++ app/src/controllers/product.controller.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const Product = require('../models/product.model');
+const Category = require('../models/category.model');
 
 const crudController = require('./crud.controller');
 
@@ -18,14 +19,27 @@ router.get('/:id', async (req, res) => {
     
     try {
         const product = await Product.findById(req.params.id).lean().exec();
-        
+        const category = await Category.findById(product.categoryId).lean().exec();
         res.render('pages/product.ejs', {
             product: product,
+            category
         });
     } catch (err) {
         return res.status(400).send({err})
     }
 });
 
+router.get('/categories/:id', async (req, res) => {
+    
+    try {
+        const category = await Product.find({categoryId: req.params.id}).lean().exec();
+        return res.send(category);
+        res.render('pages/category.ejs', {
+            category
+        });
+    } catch (err) {
+        return res.status(400).send({err})
+    }
+});
 
 module.exports = router;
